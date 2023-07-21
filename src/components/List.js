89c2d1c3acf9c6552d6b4c564efcc2a605a7c4d8
“Natacha” <aina2natacha@gmail.com>
2023-07-21 07:33:13 +0300

    thead and tbody on table

--- src/components/List.js
+++ src/components/List.js
@@ -6,25 +6,29 @@ export default function List({myList, onDelete, handleChangeItem}) {
         <div className=''>
             <h2 className='mb-2'>Your Lists</h2>
             <table className='myTable w-full border-neutral-700 text-center'>
-                <tr className='header'>
-                    <th className=''>Numéro</th>
-                    <th>Tasks</th>
-                    <th>Action</th>
-                </tr>
-                {
-                    myList.map((item, index) => {
-                        return(
-                            <tr className='myTable '>
-                                <td>{index+1}</td>
-                                <td>{item}</td>
-                                <td className='flex space-x-2 justify-center '>
-                                    <button type='button' className='bg-green-300 px-3 text-black mt-2 text-xs rounded-md' onClick={() => handleChangeItem(index)}>Edit</button>
-                                    <button type='button' className='bg-red-400 px-3  text-black selection:px-2 mt-2 text-xs rounded-md' onClick={() => onDelete(index)}>Delete</button>
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
+                <thead>
+                    <tr className='header'>
+                        <th className=''>Numéro</th>
+                        <th>Tasks</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        myList.map((item, index) => {
+                            return(
+                                <tr className='myTable '>
+                                    <td>{index+1}</td>
+                                    <td>{item}</td>
+                                    <td className='flex space-x-2 justify-center '>
+                                        <button type='button' className='bg-green-300 px-3 text-black mt-2 text-xs rounded-md' onClick={() => handleChangeItem(index)}>Edit</button>
+                                        <button type='button' className='bg-red-400 px-3  text-black selection:px-2 mt-2 text-xs rounded-md' onClick={() => onDelete(index)}>Delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
             </table>
         </div>
     )
